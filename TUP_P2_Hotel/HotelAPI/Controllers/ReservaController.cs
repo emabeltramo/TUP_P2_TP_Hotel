aa33ce88d9d0d62daee988b48f9a6c811bf0c233
Fedegg2 <fedegonzalezgiordano123456789@gmail.com>
2023-11-11 12:32:24 -0300

    ControllersApi->GetProvincias,GetLocalidades,GetHoteles

--- TUP_P2_Hotel/HotelAPI/Controllers/ReservaController.cs
+++ TUP_P2_Hotel/HotelAPI/Controllers/ReservaController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using HotelBackEnd.Front.Interface;
 using HotelBackEnd.Front.Implementation;
-
+using HotelBackEnd.Model;
 
 namespace HotelAPI.Controllers
 {
@@ -15,5 +15,45 @@ namespace HotelAPI.Controllers
         {
             front = new ReservaFront();
         }
+        public IActionResult GetProvincias()
+        {
+            List<ProvinciaModel> lstProv;
+            try
+            {
+                lstProv = front.GetProvincia();
+                return Ok(lstProv);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Error Interno!! Intente luego");
+            }
+        }
+        public IActionResult GetLocalidades ()
+        {
+            List<LocalidadModel> lstLoc;
+            try
+            {
+                lstLoc = front.GetLocalidad();
+                return Ok(lstLoc);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Error Interno!! Intente luego");
+            }
+        }
+        public IActionResult GetHoteles()
+        {
+            List<HotelModel> lstHotel;
+            try
+            {
+                lstHotel = front.GetHoteles();
+                return Ok(lstHotel);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Error Interno!! Intente luego");
+            }
+        }
+
     }
 }
