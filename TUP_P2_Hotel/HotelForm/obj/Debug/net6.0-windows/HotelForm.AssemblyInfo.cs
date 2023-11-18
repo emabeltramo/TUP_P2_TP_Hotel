547cc013d78763705f47cd9c6a42cc37f0018ff5
Mateo Bonansea <mateo.bonansea@gmail.com>
2023-11-18 19:53:50 -0300

    Borrar en proceso... cansado

--- TUP_P2_Hotel/HotelForm/obj/Debug/net6.0-windows/HotelForm.AssemblyInfo.cs
+++ TUP_P2_Hotel/HotelForm/obj/Debug/net6.0-windows/HotelForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aee0df22f565f6de09029b3fd2951f47133779e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
